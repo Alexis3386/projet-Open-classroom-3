c40afe149e991ff7b9fc8b4633629b76cb85b596
Alexis Mathiot <alesxis.mathiot@gmail.com>
2022-04-15 19:28:54 +0200

    page programmation

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	let images = ['batmanRise.jpg', 'forestGump.jpg', 'Intouchables_reference.jpg', 'killBill.jpg', 'affranchis.jpg', 'pulpFiction.jpg'];
+	let images = ['batmanRise.jpg', 'forestGump.jpg', 'Intouchables_reference.jpg', 'killBill.jpg', 'affranchis.jpg', 'pulpFiction.jpg', 'bacNord.jpg', 'nobody.jpg', 'wayofDragon.jpg', 'lesEvades.jpg', 'retourDuRoi.jpg', 'fightClub.jpg'];
 	let nb_images = images.length;
 	for (let i = 0; i < nb_images; i++) {
 		let div = $('<div class="item-images"></div>');
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 		items : 4,
 		loop: true,
-		margin: 20,
+		margin: 30,
 		nav: true,
 		autoWidth: true,
 		dots: false,
