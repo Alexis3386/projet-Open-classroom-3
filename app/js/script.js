502f55ede7dbf4d4044a932a0fd1c2ca7dfedd49
Alexis Mathiot <alesxis.mathiot@gmail.com>
2022-04-15 14:38:04 +0200

    ajout carrousel affiche de film

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,19 @@
-console.log('HELLO');
+$(document).ready(function(){
 
-const test = () => {
-	console.log('this is a test');
-};
+	let images = ['batmanRise.jpg', 'forestGump.jpg', 'Intouchables_reference.jpg', 'killBill.jpg', 'affranchis.jpg', 'pulpFiction.jpg'];
+	let nb_images = images.length;
+	for (let i = 0; i < nb_images; i++) {
+		let div = $('<div class="item-images"></div>');
+		div.css('background-image', `url('/dist/images/${images[i]}'`);
+		$("#carrousel").append(div);
+	}
+
+	$(".owl-carousel").owlCarousel({
+		items : 4,
+		loop: true,
+		margin: 20,
+		nav: true,
+		autoWidth: true,
+		dots: false,
+	});
+  });
