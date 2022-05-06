2b0974e8ba99a92babf793d5a91b07017ef9e51f
Alexis Mathiot <alesxis.mathiot@gmail.com>
2022-05-06 10:28:35 +0200

    ajout responsive mobile

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 	let images = ['batmanRise.jpg', 'forestGump.jpg', 'Intouchables_reference.jpg', 'killBill.jpg', 'affranchis.jpg', 'pulpFiction.jpg', 'bacNord.jpg', 'nobody.jpg', 'wayofDragon.jpg', 'lesEvades.jpg', 'retourDuRoi.jpg', 'fightClub.jpg'];
 	let nb_images = images.length;
@@ -9,11 +9,44 @@ $(document).ready(function(){
 	}
 
 	$(".owl-carousel").owlCarousel({
-		items : 4,
+		items: 4,
 		loop: true,
 		margin: 30,
 		nav: true,
 		autoWidth: true,
 		dots: false,
 	});
-  });
+});
+
+let boutonMobile = document.getElementById('menu-btn');
+let lienMenu = document.querySelector('.menu-mobile ul');
+
+boutonMobile.addEventListener('click', function () {
+	lienMenu.className === 'hide' ? lienMenu.className = '' : lienMenu.className = 'hide'
+});
+
+let modal = null
+
+const openModal = function (e) {
+	e.preventDefault()
+	const target = document.querySelector('#modalBatmanRise')
+	target.style.display = null
+	target.removeAttribute('aria-hidden')
+	target.setAttribute('aria-modal', 'true')
+	modal = target
+	modal.addEventListener('click', closeModal)
+}
+
+const closeModal = function (e) {
+	e.preventDefault()
+	modal.style.display = 'none'
+	target.setAttribute('aria-hidden', 'true')
+	target.removeAttribute('aria-modal')
+	modal = target
+	modal.removeEventListener('click', closeModal)
+	modal = null
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+	a.addEventListener('click', openModal)
+})
